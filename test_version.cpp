7444dd9d6eaa61f63fc5720357eafd18a4de8a83
Eugene Palchukovsky <palchukovsky@users.noreply.github.com>
2023-08-30 21:47:52 +0300

    Fix test

--- test_version.cpp
+++ test_version.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(version() == 100);
+    BOOST_CHECK(version() != 100);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
